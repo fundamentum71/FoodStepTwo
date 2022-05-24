7bd22d864e68a3dedb7ce2b0938151e5b793bc53
Aleksandr <it.kucherov@gmail.com>
2022-05-24 20:35:05 +0300

    use REST operator

--- dist/js/script.js
+++ dist/js/script.js
@@ -218,7 +218,8 @@ window.addEventListener('DOMContentLoaded', () => {
     if (e.code === 'Escape' && modal.classList.contains('show')) {
       closeModal();
     }
-  }); //const modalTimerId = setTimeout(openModal, 5000);
+  });
+  const modalTimerId = setTimeout(openModal, 5000);
 
   function showModalByScroll() {
     if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
@@ -230,12 +231,13 @@ window.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('scroll', showModalByScroll); //Используем классы для карточек
 
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
       this.price = price;
+      this.classes = classes;
       this.parent = document.querySelector(parentSelector);
       this.transfer = 27;
       this.changeToUAH();
@@ -247,8 +249,17 @@ window.addEventListener('DOMContentLoaded', () => {
 
     render() {
       const element = document.createElement('div');
+
+      if (this.classes.length == 0) {
+        this.element = 'menu__item';
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach(className => {
+          element.classList.add(className);
+        });
+      }
+
       element.innerHTML = `
-			<div class="menu__item">
 						<img src=${this.src} alt=${this.alt} />
 						<h3 class="menu__item-subtitle">${this.title}"</h3>
 						<div class="menu__item-descr">
@@ -259,7 +270,6 @@ window.addEventListener('DOMContentLoaded', () => {
 							<div class="menu__item-cost">Цена:</div>
 							<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
 						</div>
-					</div>
 			`;
       this.parent.append(element);
     }
@@ -267,8 +277,8 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   new MenuCard('img/tabs/vegy.jpg', 'vega', "Меню 'Фитнес'", "Меню 'Фитнес' - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!", 9, '.menu .container').render();
-  new MenuCard('img/tabs/elite.jpg', 'elite', "Меню 'Премиум'", "В меню 'Премиум' мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!", 14, '.menu .container').render();
-  new MenuCard('img/tabs/post.jpg', 'post', "Меню 'Постное'", "Меню 'Постное' - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.", 21, '.menu .container').render();
+  new MenuCard('img/tabs/elite.jpg', 'elite', "Меню 'Премиум'", "В меню 'Премиум' мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!", 14, '.menu .container', 'menu__item').render();
+  new MenuCard('img/tabs/post.jpg', 'post', "Меню 'Постное'", "Меню 'Постное' - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.", 21, '.menu .container', 'menu__item').render();
 });
 
 /***/ })
--- src/js/main.js
+++ src/js/main.js
@@ -134,7 +134,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	});
 
-	//const modalTimerId = setTimeout(openModal, 5000);
+	const modalTimerId = setTimeout(openModal, 5000);
 
 	function showModalByScroll() {
 		if (
@@ -151,12 +151,13 @@ window.addEventListener('DOMContentLoaded', () => {
 	//Используем классы для карточек
 
 	class MenuCard {
-		constructor(src, alt, title, descr, price, parentSelector) {
+		constructor(src, alt, title, descr, price, parentSelector, ...classes) {
 			this.src = src;
 			this.alt = alt;
 			this.title = title;
 			this.descr = descr;
 			this.price = price;
+			this.classes = classes;
 			this.parent = document.querySelector(parentSelector);
 			this.transfer = 27;
 			this.changeToUAH();
@@ -168,8 +169,17 @@ window.addEventListener('DOMContentLoaded', () => {
 
 		render() {
 			const element = document.createElement('div');
+
+			if (this.classes.length == 0) {
+				this.element = 'menu__item';
+				element.classList.add(this.element);
+			} else {
+				this.classes.forEach((className) => {
+					element.classList.add(className);
+				});
+			}
+
 			element.innerHTML = `
-			<div class="menu__item">
 						<img src=${this.src} alt=${this.alt} />
 						<h3 class="menu__item-subtitle">${this.title}"</h3>
 						<div class="menu__item-descr">
@@ -180,7 +190,6 @@ window.addEventListener('DOMContentLoaded', () => {
 							<div class="menu__item-cost">Цена:</div>
 							<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
 						</div>
-					</div>
 			`;
 			this.parent.append(element);
 		}
@@ -201,7 +210,8 @@ window.addEventListener('DOMContentLoaded', () => {
 		"Меню 'Премиум'",
 		"В меню 'Премиум' мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
 		14,
-		'.menu .container'
+		'.menu .container',
+		'menu__item'
 	).render();
 
 	new MenuCard(
@@ -210,6 +220,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		"Меню 'Постное'",
 		"Меню 'Постное' - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
 		21,
-		'.menu .container'
+		'.menu .container',
+		'menu__item'
 	).render();
 });
