7b5aaf9c847e7592804345fb7d88670e3fced053
Aleksandr <it.kucherov@gmail.com>
2022-06-02 20:30:06 +0300

    practic fetch()

--- src/js/main.js
+++ src/js/main.js
@@ -249,10 +249,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
 			form.insertAdjacentElement('afterend', statusMessage);
 
-			const request = new XMLHttpRequest();
-			request.open('POST', 'server.php');
-
-			request.setRequestHeader('Content-type123', 'application/json');
 			const formData = new FormData(form);
 
 			const object = {};
@@ -261,20 +257,27 @@ window.addEventListener('DOMContentLoaded', () => {
 				object[key] = value;
 			});
 
-			const json = JSON.stringify(object);
-
-			request.send(json);
-
-			request.addEventListener('load', () => {
-				if (request.status === 200) {
-					console.log(request.response);
+			fetch('server.php', {
+				method: 'POST',
+				headers: {
+					'Content-type': 'application/json',
+				},
+				body: JSON.stringify(object),
+			})
+				.then((data) => data.text())
+				.then((data) => {
+					console.log(data);
 					showThanksModal(message.success);
 					form.reset();
 					statusMessage.remove();
-				} else {
+				})
+				.catch(() => {
 					showThanksModal(message.failure);
-				}
-			});
+					form.reset();
+				});
+			//.finally(() => {
+			//	form.reset();
+			//});
 		});
 	}
 
